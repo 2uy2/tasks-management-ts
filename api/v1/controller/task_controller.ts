d5b5513c3f21b7e716b5f3481ef128f5479b428a
2uy2 <quytran11204@gmail.com>
2025-12-04 11:40:24 +0700

    xắp xếp theo tiêu chí

--- api/v1/controller/task_controller.ts
+++ api/v1/controller/task_controller.ts
@@ -13,7 +13,14 @@ export const index =async (req:Request,res:Response)=>{
     if(req.query.status){
         find.status=req.query.status.toString();
     }
-    const task = await Task.find(find)
+    const sort = {};
+    if(req.query.sortKey && req.query.sortValue){
+        const sortKey=req.query.sortKey.toString();
+        const sortValue=req.query.sortValue.toString();
+        sort[sortKey]=sortValue;
+
+    }
+    const task = await Task.find(find).sort(sort)
     console.log(task);
 
     res.json(task)
