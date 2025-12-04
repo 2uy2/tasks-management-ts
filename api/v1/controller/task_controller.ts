0fa338bb72a0c864f674d59a83d81adc9b3473f9
2uy2 <quytran11204@gmail.com>
2025-12-04 11:17:21 +0700

    bộ lọc theo trạng thái

--- api/v1/controller/task_controller.ts
+++ api/v1/controller/task_controller.ts
@@ -2,9 +2,18 @@ import Task from "../model/task_model";
 import  {Router,Request,Response} from "express";
 
 export const index =async (req:Request,res:Response)=>{
-    const task = await Task.find({
+    interface Find {
+        deleted: boolean,
+        status? : string,
+
+    }
+    const find: Find={
         deleted:false,
-    })
+    }
+    if(req.query.status){
+        find.status=req.query.status.toString();
+    }
+    const task = await Task.find(find)
     console.log(task);
 
     res.json(task)
